6ce3404e937ab3eefbb70a1333732960f5a419d5
safin-devloper <safin1089@gmail.com>
2026-09-17 22:46:04 +0600

    added responsive navbar and setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './component/Nav';
+import Banner from './component/Banner';
+
+const App = () => {
+  return (
+    <div>
+ <Nav/>
+ <Banner/>
+      
+    </div>
+  );
+};
+
+export default App;
